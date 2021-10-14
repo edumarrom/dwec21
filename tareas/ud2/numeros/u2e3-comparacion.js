396d755db618bcd06b7db3ceb306af32e428431a
Eduardo Martínez <emartinezr161@outlook.com>
2021-10-14 12:40:02 +0200

    corregido ejercicio u2e3

--- tareas/ud2/numeros/u2e3-comparacion.js
+++ tareas/ud2/numeros/u2e3-comparacion.js
@@ -23,22 +23,38 @@ Las operaciones a realizar son:
 
 Comenta el código con los comentarios que estimes necesarios.
 */
+
+/* TODO: Cambiar comillas dobles por simples. */
+let operacion1 = 10 == 10;
+// Los enteros primitivos del mismo valor son siempreel mismo entero.
+let operacion2 = 10 === 10;
+let operacion3 = 10 === 10.0;         // La parte decimal es truncada.
+let operacion4 = 'Laura' == 'laura';  // 'L' != 'l'
+let operacion5 = 'Laura' > 'laura';   // Minúsculas primero
+let operacion6 = 'Laura' < 'laura';   // 'a' > 'A'
+let operacion7 = '123' == 123;        // Comparación permisiva
+let operacion8 = '123' === 123;       // Comparación estricta
+let operacion9 = parseInt('123') === 123; // Si lo ejecuto en consola es true
+
 let operaciones = [
-    '10 == 10',
-    '10 === 10',
-    '10 === 10.0',
-    '“Laura” == “laura”',
-    '“Laura” > “laura”',
-    '“Laura” < “laura”',
-    '“123” == 123',
-    '“123” === 123',
-    'parseInt(“123”) === 123'
+    operacion1, operacion2,
+    operacion3, operacion4,
+    operacion5, operacion6,
+    operacion7, operacion8
 ]
 
-function mostrarOperacion(operacion) {
-    alert ('La operación ' + operaciones[0]+ ' es ' + operacion);
+let enunciados = [
+    '10 == 10', '10 === 10',
+    '10 === 10.0', "'Laura' == 'laura'",
+    "'Laura' > 'laura'", "'Laura' < 'laura'",
+    "'123' == 123", "'123' === 123",
+    "parseInt('123') === 123"
+]
+
+function mostrarOperacion(enunciado, operacion) {
+    alert (`La operacion ${enunciado} es ${operacion}`);
 }
 
-operaciones.forEach(e => {
-    mostrarOperacion(e);
-});
+for (let i = 0; i < enunciados.length; i++) {
+    mostrarOperacion(enunciados[i], operaciones[i]);
+}
